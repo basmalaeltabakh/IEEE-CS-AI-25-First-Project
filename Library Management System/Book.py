a8469645d55be862546c2b1ef44c965e0e2ce839
Basmala Saeed Eltabakh <basmalaeltabakh49@gmail.com>
2025-02-11 17:43:28 +0200

    creating Book class

---
+++ Library Management System/Book.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class Book :
+    
+    def init(self , id , title , author , publicationYear):
+        self.id = id 
+        self.title = title
+        self.author = author
+        self.publicationYear = publicationYear
+    
+    #Function To Display Book Details
+    def DisplayBookDetails(self):
+        print("\t\t Book Details ")
+        print("ID:", self.id)
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Publication Year:", self.publicationYear)
+    
+    # Display Message box with Book Details
+    def DisplayMessageBox(self):
+         Bookdetails = f"ID: {self.id}\nTitle: {self.title}\nAuthor: {self.author}\nPublication Year: {self.publicationYear}"
+         messagebox.showinfo("Book Details", Bookdetails)
